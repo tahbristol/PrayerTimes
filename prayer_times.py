2dd735fb5d449d48f7eacf2d3f344c5ddfc65eb1
tahbristol <tahbristol@gmail.com>
2018-12-21 23:47:10 -0500

    get_address_from_ip test and logic, get_prayer_times logic

--- prayer_times.py
+++ prayer_times.py
@@ -11,7 +11,22 @@ def get_ip_address():
     return response.json()['ip']
     
 def get_address_from_ip(ip):
-    pass
+    api_key = 'b1978ca33eb7b1733eca5503f8046b49'
+    url = f'http://api.ipstack.com/{ip}?access_key={api_key}'
+    
+    try:
+        response = req.get(url).json()
+    except Exception as e:
+        raise e
+    
+    return f"city={response['city']}&state={response['region_code']}&country={response['country_name']}"
     
 def get_prayer_times(address):
-    pass
+    url = 'http://api.aladhan.com/v1/timingsByCity?'
+    
+    try:
+        response = req.get(url + address).json()
+    except Exception as e:
+        raise e
+    
+    return response['data']['timings']
--- prayer_times_tests.py
+++ prayer_times_tests.py
@@ -9,7 +9,15 @@ class TestPrayerTimes(unittest.TestCase):
         ip = get_ip_address()
         
         self.assertIsNotNone(ip)
-
-
+     
+    def test_get_address_from_ip(self):
+        ip = '67.174.76.53'
+        address = get_address_from_ip(ip)
+        
+        self.assertIn('city', address)
+        self.assertIn('state', address)
+        self.assertIn('country', address)
+        
+        
 if __name__=='__main__':
     unittest.main()
