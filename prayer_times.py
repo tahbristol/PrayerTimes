591085ef77f2223b7198679f42ab2af0ebe5c66d
tahbristol <tahbristol@gmail.com>
2018-12-21 22:39:35 -0500

    get_ip_address logic

--- prayer_times.py
+++ prayer_times.py
@@ -2,11 +2,16 @@ import requests as req
 
 
 def get_ip_address():
-    pass
+    
+    try:
+        response = req.get('https://api.ipify.org?format=json')
+    except Exception as e:
+        raise e
+    
+    return response.json()['ip']
     
 def get_address_from_ip(ip):
     pass
     
 def get_prayer_times(address):
     pass
-    
