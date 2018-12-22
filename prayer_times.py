63d0ef3d0f254d633147b5feb73a04d1bc8d47b2
tahbristol <tahbristol@gmail.com>
2018-12-21 22:31:41 -0500

    initial commit

---
+++ prayer_times.py
@@ -0,0 +1,12 @@
+import requests as req
+
+import pdb; pdb.set_trace()
+def get_ip_address():
+    pass
+    
+def get_address_from_ip(ip):
+    pass
+    
+def get_prayer_times(address):
+    pass
+    
